31971cd5eb2a97da8a6bc06c4a1267a05bdbe61a
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-04-28 17:15:26 +0600

    top header top section design done

--- InstaGramClone/src/App.tsx
+++ InstaGramClone/src/App.tsx
@@ -1,6 +1,9 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faMugSaucer,faAngleDown, faHeart,faPaperPlane} from '@fortawesome/free-solid-svg-icons/'
+
 import {
   SafeAreaView,
   ScrollView,
@@ -9,6 +12,7 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -28,7 +32,22 @@ function App(): JSX.Element {
    <SafeAreaView>
     <View style={styles.MainContainer}>
       <View style = {styles.headerSection}>
-      <View style={styles.topHeader}></View>
+      <View style={styles.topHeader}>
+        <View style={styles.topHeaderleft}>
+          <Text style={styles.logo}>Instagram</Text>
+     
+          <FontAwesomeIcon style={styles.logo_down} icon={faAngleDown} />
+
+        </View>
+        <View style={styles.topHeaderRight}>
+        <TouchableOpacity> 
+          <FontAwesomeIcon size={18} style={styles.logo_downLove} icon={faHeart} />
+          </TouchableOpacity>
+        <TouchableOpacity> 
+          <FontAwesomeIcon size={18} style={styles.logo_downSend} icon={faPaperPlane} />
+        </TouchableOpacity>
+        </View>
+      </View>
       <View style={styles.bottomHeader}></View>
       </View>
       <View style = {styles.footerSection}>
@@ -43,12 +62,70 @@ const styles = StyleSheet.create({
   MainContainer:{
     width:"100%",
     height:"100%",
-    backgroundColor:"#0D0D0D"
+    backgroundColor:"#0D0D0D",
+    // flex:1,
+    flexDirection:"row",
   },
-  headerSection:{},
+  headerSection:{
+    width:"100%",
+    height:150,
+    borderWidth:1,
+    
+  },
   footerSection:{},
-  topHeader:{},
+  topHeader:{
+    flex:1,
+    width:"100%",
+    height:50,
+    flexDirection:"row",
+    borderColor:"#FFF",
+    
+    
+  },
   bottomHeader:{},
+  topHeaderleft:{
+    width:"50%",
+    height:50,
+    paddingTop:10,
+    flexDirection:"row",
+  },
+topHeaderRight:{
+  width:"50%",
+  height:50,
+  borderColor:"#E21717",
+  flexDirection:'row',
+  justifyContent:"flex-end",
+  // paddingTop:15,
+  paddingRight:20,
+},
+logo:{
+  color:"#FFF",
+  fontWeight:"bold",
+  fontSize:17,
+  marginLeft:10,
+
+  
+},
+logo_down:{
+  marginTop:5,
+  marginLeft:3,
+  color:"#FFF",
+},
+logo_downLove:{
+  marginTop:15,
+  marginLeft:3,
+  color:"#FFF",
+
+},
+logo_downSend:{
+  marginTop:15,
+  marginLeft:10,
+  color:"#FFF",
+  
+},
+iconstyle:{
+  
+}
 });
 
 export default App;
